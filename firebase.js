37c07db10a7a87701c9b1cd41d3d97fda1068d96
tumeminem191za-ux <tumeminem191za@gmail.com>
2026-09-09 03:27:36 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAT5o9FpRc6cWFW9oPdYrFJHEU-pIHjbH4",
+  authDomain: "ivy-auction.firebaseapp.com",
+  projectId: "ivy-auction",
+  storageBucket: "ivy-auction.firebasestorage.app",
+  messagingSenderId: "1079698381099",
+  appId: "1:1079698381099:web:9ea830e4b4f1fdafcc4d6e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
