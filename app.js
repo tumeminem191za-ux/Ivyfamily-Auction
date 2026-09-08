ee22333d747299af6272acb6c56e97dcf7f62556
tumeminem191za-ux <tumeminem191za@gmail.com>
2026-09-09 03:59:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,75 @@
+import { db } from "./firebase.js";
+import { 
+  collection, 
+  addDoc, 
+  getDocs 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 let data=[];
-function add(){
- let x={name:name.value,item:item.value,page:page.value,piece:piece.value};
- data.push(x);render();
+
+window.add = async function(){
+
+ let x={
+  name: document.getElementById("name").value,
+  item: document.getElementById("item").value,
+  page: document.getElementById("page").value,
+  piece: document.getElementById("piece").value
+ };
+
+ await addDoc(collection(db,"bids"),x);
+
+ alert("ลงชื่อแล้ว");
+ load();
 }
+
+
+async function load(){
+
+ data=[];
+
+ let snap=await getDocs(collection(db,"bids"));
+
+ snap.forEach(doc=>{
+   data.push(doc.data());
+ });
+
+ render();
+}
+
+
 function render(){
- list.innerHTML=data.map(x=>`${x.name} | ${x.item} | ${x.page} | ชิ้น ${x.piece}`).join('<br>');
+
+ document.getElementById("list").innerHTML =
+ data.map(x=>
+ `${x.name} | ${x.item} | ${x.page} | ชิ้น ${x.piece}`
+ ).join("<br>");
+
 }
-function wheel(){
- let target=prompt('ใส่ชื่อไอเทมที่จะหมุน');
- let a=[...new Set(data.filter(x=>x.item==target).map(x=>x.name))];
- if(a.length<2){result.innerHTML='ไม่มีการแย่ง';return}
+
+
+window.wheel=function(){
+
+ let target=prompt("ใส่ชื่อไอเทมที่จะหมุน");
+
+ let a=[
+ ...new Set(
+  data
+  .filter(x=>x.item==target)
+  .map(x=>x.name)
+ )
+ ];
+
+ if(a.length<2){
+  document.getElementById("result").innerHTML="ไม่มีการแย่ง";
+  return;
+ }
+
  let w=a[Math.floor(Math.random()*a.length)];
- result.innerHTML='ผู้ได้สิทธิ์: '+w;
+
+ document.getElementById("result").innerHTML=
+ "ผู้ได้สิทธิ์: "+w;
+
 }
+
+
+load();
