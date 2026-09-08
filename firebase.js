1e3a86a1bf21057f6957e22509eddd59c1379337
tumeminem191za-ux <tumeminem191za@gmail.com>
2026-09-09 03:47:51 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAT5o9FpRc6cWFW9oPdYrFJHEU-pIHjbH4",
   authDomain: "ivy-auction.firebaseapp.com",
@@ -13,5 +10,6 @@ const firebaseConfig = {
   appId: "1:1079698381099:web:9ea830e4b4f1fdafcc4d6e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
