0b4a16ddbcc144ef1bac51ba53b8ce59b6fe1fbf
tumeminem191za-ux <tumeminem191za@gmail.com>
2026-09-09 04:05:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,119 @@
 import { db } from "./firebase.js";
-import { 
-  collection, 
-  addDoc, 
-  getDocs 
+
+import {
+  collection,
+  addDoc,
+  onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-let data=[];
 
+let data = [];
+
+
+// ส่งชื่อประมูล
 window.add = async function(){
 
- let x={
-  name: document.getElementById("name").value,
-  item: document.getElementById("item").value,
-  page: document.getElementById("page").value,
-  piece: document.getElementById("piece").value
- };
+  let x = {
+    name: document.getElementById("name").value,
+    item: document.getElementById("item").value,
+    page: document.getElementById("page").value,
+    piece: document.getElementById("piece").value,
+    time: new Date()
+  };
 
- await addDoc(collection(db,"bids"),x);
 
- alert("ลงชื่อแล้ว");
- load();
+  if(!x.name){
+    alert("กรุณาใส่ชื่อ");
+    return;
+  }
+
+
+  await addDoc(collection(db,"bids"),x);
+
+
+  alert("ลงชื่อแล้ว");
+
+  document.getElementById("name").value="";
+  document.getElementById("piece").value="";
+};
+
+
+// ดึงข้อมูลสดจาก Firebase
+function load(){
+
+  onSnapshot(collection(db,"bids"),(snap)=>{
+
+    data=[];
+
+
+    snap.forEach(doc=>{
+
+      data.push(doc.data());
+
+    });
+
+
+    render();
+
+  });
+
 }
 
 
-async function load(){
-
- data=[];
-
- let snap=await getDocs(collection(db,"bids"));
-
- snap.forEach(doc=>{
-   data.push(doc.data());
- });
-
- render();
-}
-
-
+// แสดงรายชื่อ
 function render(){
 
- document.getElementById("list").innerHTML =
- data.map(x=>
- `${x.name} | ${x.item} | ${x.page} | ชิ้น ${x.piece}`
- ).join("<br>");
+  let list=document.getElementById("list");
+
+
+  list.innerHTML=data.map(x=>`
+
+    ${x.name} | ${x.item} | ${x.page} | ชิ้น ${x.piece}
+
+  `).join("<br>");
 
 }
 
 
+
+// วงล้อสุ่ม
 window.wheel=function(){
 
- let target=prompt("ใส่ชื่อไอเทมที่จะหมุน");
+  let target=prompt("ใส่ชื่อไอเทมที่จะหมุน");
 
- let a=[
- ...new Set(
-  data
-  .filter(x=>x.item==target)
-  .map(x=>x.name)
- )
- ];
 
- if(a.length<2){
-  document.getElementById("result").innerHTML="ไม่มีการแย่ง";
-  return;
- }
+  let a=[
 
- let w=a[Math.floor(Math.random()*a.length)];
+    ...new Set(
 
- document.getElementById("result").innerHTML=
- "ผู้ได้สิทธิ์: "+w;
+      data
+      .filter(x=>x.item==target)
+      .map(x=>x.name)
+
+    )
+
+  ];
+
+
+  if(a.length<2){
+
+    document.getElementById("result").innerHTML=
+    "ไม่มีการแย่ง";
+
+    return;
+
+  }
+
+
+  let w=a[Math.floor(Math.random()*a.length)];
+
+
+  document.getElementById("result").innerHTML=
+  "ผู้ได้สิทธิ์: "+w;
+
 
 }
 
 
+// เริ่มโหลดข้อมูล
 load();
